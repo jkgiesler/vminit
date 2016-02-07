1b0b9f9df24a678b3bd97d0ad4f5d3944b964a41
jkgiesler <jason.giesler@gmail.com>
2016-02-07 11:18:50 -0600

    make sure vim is installed

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get upgrade -y
 
 apt-get install -y r-base
-apt-get install -y build-essential cmake python-dev
+apt-get install -y vim build-essential cmake python-dev
 
 Rscript install.R
 
